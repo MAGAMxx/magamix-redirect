8424196dd20e78d6992e0ab6e9f5cdf3cc23a65a
MAGAMxx <mohammadakubov@gmail.com>
2026-01-12 04:53:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 
+// Простой редирект на подписку
 app.get('/connect/:code', (req, res) => {
   const code = req.params.code;
-  res.redirect(302, `https://31.130.131.214:2096/sub/${code}`);  // ← https + порт 2096
+  res.redirect(302, `https://31.130.131.214:2096/sub/${code}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('OK - Redirect работает! Используй /connect/твой_sub_id');
+// Обёртка для Happ deeplink (как у конкурентов)
+app.get('/url', (req, res) => {
+  const happUrl = req.query.url;  // ?url=happ://add/...
+  if (happUrl && happUrl.startsWith('happ://add/')) {
+    res.redirect(302, happUrl);  // редирект на happ://
+  } else {
+    res.status(400).send('Bad Request');
+  }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Запущен на ${port}`);
+  console.log(`Сервер на ${port}`);
 });
