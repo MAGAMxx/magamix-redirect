d0f16f419ffa0007e2f76d9e75490b3185eb936f
MAGAMxx <mohammadakubov@gmail.com>
2026-01-12 03:43:32 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/connect/:code', (req, res) => {
+  const code = req.params.code;
+  res.redirect(302, `http://31.130.131.214/sub/${code}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('OK - Redirect работает! Используй /connect/твой_sub_id');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Запущен на ${port}`);
+});
